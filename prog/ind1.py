a0ff8f4b3455c97adb2d46d655f02898c9826421
bebebrrr <kubanova04@gmail.com>
2023-12-09 21:37:23 +0300

    individual program

--- prog/ind1.py
+++ prog/ind1.py
@@ -13,5 +13,13 @@ if __name__ == '__main__':
     for i in range(1, len(a) + 1, 2):
      if a[i-1] % 2 != 0 and a[i] % 2 != 0:
         count += 1
-    print(count)   
-   
+            
+
+    for i in range(1, len(a) + 1, 2):
+        if a[i-1] % 2 != 0 and a[i] % 2 != 0:
+            p1=i - 1
+            p2=i 
+            break
+
+    print("Количество нечётных пар: ", count)
+    print("Индексы нечётной пары: ", p1, p2)   
